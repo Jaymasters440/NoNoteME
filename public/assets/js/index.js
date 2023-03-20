43c74ac88552960ae2b940dd0901c7b89fe9a9d5
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-03-20 13:25:57 -0800

    Add functions to server.js index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -52,7 +52,7 @@ const deleteNote = (id) =>
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
-
+  //console.log("rendering +" + activeNote.text +activeNote.title +activeNote.id);
   if (activeNote.id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('./public/assets/helpers/fsUtils');
+
+//const api = require('./public/assets/js/notes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api/notes', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>{
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+    var targetId = req.params.id
+    var content = ""
+    readFromFile('./db/db.json').then((data) => {
+        let jsonNotes = (JSON.parse(data));
+        jsonNotes.forEach((note) => {
+            if (note.id !== targetId){
+                content += note 
+            }
+          });
+          writeToFile ('./db/db.json', content )
+    });
+})
+
+app.post('/api/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        "title":title,
+        "text": text,
+        "id": uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+
+  app.get('/api/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
